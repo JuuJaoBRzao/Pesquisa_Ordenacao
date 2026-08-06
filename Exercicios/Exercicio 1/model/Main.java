37673dde3141ccc05be72bd3884f98e74f3a5499
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-08-06 10:41:26 -0300

    Delete Exercicios/Exercicio 1/model/Main.java

--- Exercicios/Exercicio 1/model/Main.java
+++
@@ -1,6 +0,0 @@
-import controller.MedicaoController;
-
-public static void main(String[] args) {
-        MedicaoController controller = new MedicaoController();
-        controller.executar();
-    }
