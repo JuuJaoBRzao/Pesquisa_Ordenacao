d00f572a072b493ce376ae7124c4688c789ebb2b
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-08-06 10:40:44 -0300

    Create Main.java

---
+++ Exercicios/Exercicio 1/model/Main.java
@@ -0,0 +1,6 @@
+import controller.MedicaoController;
+
+public static void main(String[] args) {
+        MedicaoController controller = new MedicaoController();
+        controller.executar();
+    }
